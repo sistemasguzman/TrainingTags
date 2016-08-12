6b2f5bc57711c5e92851ae687b525d830f927e97
Esteban Guzmán Zerafín <eguzman@ia.com.mx>
2016-08-12 14:48:37 -0500

    attributos para mongodb

--- TrainingTag/TrainingTagCmd/Models/Movie.cs
+++ TrainingTag/TrainingTagCmd/Models/Movie.cs
@@ -5,9 +5,14 @@ namespace TrainingTagCmd.Models
     [BsonIgnoreExtraElements]
     public class Movie
     {
+        [BsonId]
+        [BsonElement("_id")]
         public int Id { get; set; }
+        [BsonElement("title")]
         public string Title { get; set; }
+        [BsonElement("key")]
         public string Key { get; set; }
+        [BsonElement("poster")]
         public string Poster { get; set; }
     }
 }
