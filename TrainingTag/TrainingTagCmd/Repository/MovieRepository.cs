8922c98149afaa85c1c11d4ed07548a2cce78712
Esteban Guzmán Zerafín <eguzman@ia.com.mx>
2016-08-12 14:46:42 -0500

    add new MovieRepository

---
+++ TrainingTag/TrainingTagCmd/Repository/MovieRepository.cs
@@ -0,0 +1,12 @@
+﻿using TrainingTagCmd.Models;
+
+namespace TrainingTagCmd.Repository
+{
+    public class MovieRepository
+    {
+        public Movie Get()
+        {
+            return new Movie();
+        }
+    }
+}
